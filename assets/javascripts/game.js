f53bde14e7b912cc83dfda4d78fc4febd7481a11
Kyle Dreger <kyle@dreger.me>
2015-05-04 22:04:43 -0400

    updated interaction, dom now procures second btn
    second button now appears, offering player a choice. as pieces of the
    narrative are added, they fade.

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,8 +1,27 @@
+var nextMessage = "";
+var optionA = "";
+var optionB = "";
+var currentTreeProgress = 0;
+
 $( document ).ready(function() {
   $('.game-button').attr("disabled", false);
 });
 
 $('.game-button').click(function() {
-  $('.game-intro').fadeOut();
-  $(this).attr("disabled", true).val("Waiting on transmission...");
+  nextMessage = "Code word?";
+  optionA = "Sagitarious.";
+  optionB = "Sagitarious, <stupid code words>.";
+  timeout_init();
+  currentTreeProgress = currentTreeProgress + 1;
 })
+
+
+function timeout_init() {
+  $('.game-button').attr("disabled", true);
+  setTimeout(function() {
+    $('.game-narrative').prepend("<p>" + nextMessage + "</p>");
+    $('.game-button').attr("disabled", false).show();
+    $('.game-button-a').val(optionA);
+    $('.game-button-b').val(optionB);
+  }, 2000);
+}
